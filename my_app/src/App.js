559fb4dee860c7190f38459d90d91ef84c1e7c4d
shkim787 <shkim061198@gmail.com>
2020-01-15 19:47:02 +0900

    [INSERT] Listes using map, props

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './css/App.css';
 import Movie from './Movie';
 
-function App() {
-  return (
-    <div className="App">
-      <Movie />
-    </div>
-  );
+const movies = 
+[
+  {
+    title : "maxtrix",
+    poster : "https://cdn.vox-cdn.com/thumbor/lzOgDk9FJXkaA2PegCZ_Mcti6d8=/0x0:1280x720/1200x800/filters:focal(538x258:742x462)/cdn.vox-cdn.com/uploads/chorus_image/image/64917704/matrix.0.jpg"
+
+  },
+  {
+    title : "full metal jacket",
+    poster : "http://mblogthumb1.phinf.naver.net/MjAxOTAxMDNfMSAg/MDAxNTQ2NTIyNzIxMDQy.Mg-Unq0TcP20nuJt4jwQIII0qaJoxf9blEmWZryiL_Ig.Iya4LSd-MU81saiCOXhBAAaF87TMoWCTfO-pz_WjyI4g.JPEG.winpil99/full.jpg?type=w800"
+
+  },
+  {
+    title : "old boy",
+    poster : "https://upload.wikimedia.org/wikipedia/ko/thumb/4/48/Old_Boy.jpg/220px-Old_Boy.jpg"
+
+  },
+  {
+    title : "star wars",
+    poster : "https://images-na.ssl-images-amazon.com/images/I/710NixgbhyL._SY445_.jpg"
+
+  }
+]
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        {movies.map(movie => {
+          return <Movie title={movie.title} poster={movie.poster}/>
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
--- my_app/src/Movie.js
+++ my_app/src/Movie.js
@@ -5,8 +5,8 @@ class Movie extends Component{
     render(){
         return(
             <div>
-                <MoviePoster/>
-                <h1>hello this is a movie</h1>
+                <MoviePoster poster ={this.props.poster} />
+                <h1>{this.props.title}</h1>
             </div>
         )
     }
@@ -14,8 +14,9 @@ class Movie extends Component{
 
 class MoviePoster extends Component{
     render(){
+        console.log(this.props)
         return(
-            <img src="./logo192.png"></img>
+            <img src={this.props.poster}></img>
         )
     }
 }
